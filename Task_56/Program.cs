c66e4869d1258a0aa475424d71c6acf01bc14e6d
Dima Bykadorov <dimka.b@live.com>
2023-09-02 22:20:00 +0300

    Добавлено автоформатирование

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -4,35 +4,45 @@ Console.WriteLine();
 FindMinSumInRows(array);
 
 
-int[,] GetArray(int rows, int columns, int minValue, int maxValue){
-    int[,] result = new int[rows,columns];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < columns; j++){
-            result[i,j] = new Random().Next(minValue, maxValue + 1);
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
     return result;
 }
 
-void PrintArray(int[,] array){
-    for(int i = 0; i < array.GetLength(0); i++){
-        for(int j = 0; j < array.GetLength(1); j++){
-            Console.Write($"{array[i,j]} ");
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 
-void FindMinSumInRows (int[,] array){
+void FindMinSumInRows(int[,] array)
+{
     int rowNumber = 1;
     int minSum = 0;
     int tempSum = 0;
-    for(int i = 0; i < array.GetLength(0); i++){
-        for(int j = 0; j < array.GetLength(1); j++){
-            tempSum += array[i,j];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            tempSum += array[i, j];
         }
         if (i == 0) minSum = tempSum;
-        else if (tempSum < minSum) {
+        else if (tempSum < minSum)
+        {
             minSum = tempSum;
             tempSum = 0;
             rowNumber = i + 1;
